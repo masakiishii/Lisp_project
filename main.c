cae1831a75d0ba936bbf10bfedd7afc9f2365d78
masakiishii <masaki.ishii511@gmail.com>
2012-04-11 12:56:11 +0900

    add main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <readline/readline.h>
+#include "ilisp.h"
+
+int main(int argc,char **argv)
+{
+  cons_t *tree;
+  
+  tree = iread();
+  //	eval();
+  print(tree,0);
+  
+  printf("\n");
+
+  return 0;
+}
